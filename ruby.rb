5094edaa256e470407281e129773e41e62357bbf
kabita-islam <kabita.islam@technonext.com>
2024-10-14 10:19:18 +0600

    updated

---
+++ ruby.rb
@@ -0,0 +1,30 @@
+###########  variable  
+
+#a,b=10,2;
+#b=2;
+#puts a,b;
+
+# puts "ruby on rails"  
+# print "ruby on rails\n"
+
+
+##### notes: puts always gives line break + print does not give line break 
+
+
+#######   function
+
+# def add(a,b)
+#   puts a+b
+# end
+
+# add(3,4)
+
+###  loop
+
+j=1
+while j<=10
+    if j%2==0
+        puts j
+    end
+    j+=1
+end
