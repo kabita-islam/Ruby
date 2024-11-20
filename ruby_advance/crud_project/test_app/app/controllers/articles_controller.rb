f3be8acb5e6aa5de02038cf67f016fb9b9e14fda
kabita-islam <kabitaislam30@gmail.com>
2024-11-20 18:36:26 +0600

    Updated

--- ruby_advance/crud_project/test_app/app/controllers/articles_controller.rb
+++ ruby_advance/crud_project/test_app/app/controllers/articles_controller.rb
@@ -3,26 +3,48 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   
-  #render plain: params[:article].inspect
-  # @article.save
-  # redirect_to article_path(@article)
-
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Succesfully Added."
-      redirect_to article_path(@article)
-    else
-      render 'new'
+     # flash[:notice] = "Succesfully Added."
+      redirect_to articles_path(@article)
+    else 
+     render :new
     end
   end
 
   def article_params
-     params.require(:article).permit(:title,:description)
+    params.require(:article).permit(:title,:description)
   end
 
-  def articles_show
-       
+  def show
+    @article = Article.find(params[:id])   
+  end
+
+  def edit
+    @article = Article.find(params[:id]) 
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Successfully updated!"
+      redirect_to articles_path(@article)
+    else
+      render :edit
+    end
+  end
+
+
+  def index
+    @articles= Article.all
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article is Deleted Successfully."
+    redirect_to articles_path
   end
 
 end
