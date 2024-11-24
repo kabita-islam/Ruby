c845f1db1516ed42fdbb2ed406fcd82dca630ad4
kabita-islam <kabitaislam30@gmail.com>
2024-11-24 21:27:13 +0600

    updated

--- ruby_advance/crud_project/student_app/app/controllers/students_controller.rb
+++ ruby_advance/crud_project/student_app/app/controllers/students_controller.rb
@@ -23,5 +23,23 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
   end
+  def edit
+    @student = Student.find(params[:id])
+  end
+  def update
+    @student = Student.find(params[:id])
+    if @student.update(student_params)
+      flash[:notice] = "Student Information is Updated."
+      redirect_to students_path(@student)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @student = Student.find(params[:id])
+    @student.destroy
+    redirect_to student_path
+  end
 
 end
--- ruby_advance/crud_project/student_app/db/migrate/20241121103955_add_column_to_students.rb
+++ ruby_advance/crud_project/student_app/db/migrate/20241121103955_add_column_to_students.rb
@@ -1,6 +1,6 @@
 class AddColumnToStudents < ActiveRecord::Migration[7.2]
   def change
-    add_column :students, :student_id, :integer
+    add_column :students, :S_Id, :integer
     add_column :students, :department, :text
     add_column :students, :created_at, :datetime
     add_column :students, :upated_at, :datetime
