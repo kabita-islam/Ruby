7b484fcb86d4d464f108b61343119814608b1f78
kabita-islam <kabitaislam30@gmail.com>
2024-12-05 18:46:09 +0600

    addedd

--- ruby_advance/crud_project/Home_Page/app/controllers/application_controller.rb
+++ ruby_advance/crud_project/Home_Page/app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  protect_from_forgery with: :exception
   helper_method :current_user, :logged_in?, :require_user #for this it is available in views
+
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    if session[:user_id]
+      @current_user ||= begin
+        User.find(session[:user_id])
+      rescue ActiveRecord::RecordNotFound
+        session[:user_id] = nil
+        nil
+      end
+    end
   end
   def logged_in?
     !!current_user
