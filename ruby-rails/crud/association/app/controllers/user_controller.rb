be4aee461c0e1bf6ac6dbd81e29d4875ab138494
kabita-islam <kabitaislam30@gmail.com>
2025-05-15 18:28:28 +0600

    Add association folder as part of main repo

---
+++ ruby-rails/crud/association/app/controllers/user_controller.rb
@@ -0,0 +1,37 @@
+class UserController < ApplicationController
+  def index
+    @users=User.all
+  end
+  def new
+    @user=User.new
+  end
+  def create
+    @user=User.new(user_params)
+    if @user.save
+      redirect_to all_users_path (@user)
+    else
+      render "new"
+    end
+  end
+  def edit
+    @user=User.find(params[:id])
+  end
+  def update
+    @user=User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to all_users_path (@user)
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @user=User.find(params[:id])
+    @user.destroy
+    redirect_to all_users_path
+
+  end
+  def user_params
+    params.require(:user).permit(:username, :email)
+  end
+end
---
+++ ruby-rails/crud/association/config/routes.rb
@@ -0,0 +1,26 @@
+Rails.application.routes.draw do
+  scope "/article" do
+    controller :article do
+      get "/index", action: :index, as: "all_articles"
+      get "/new", action: :new, as: "articles"
+      post "/new_article", action: :create, as: "new_articles"
+      get "/edit/:id", action: :edit, as: "edit_articles"
+      patch "/update/:id", action: :update, as: "update_articles"
+      get "/show/:id", action: :show, as: "show_articles"
+      delete "/delete/:id", action: :destroy, as: "delete_articles"
+    end
+  end
+
+
+  scope "/users" do
+    controller :user do
+      get "/index", action: :index, as: "all_users"
+      get "/new", action: :new, as: "new_users"
+      post "/create", action: :create, as: "create_users"
+      get "/edit/:id", action: :edit, as: "edit_users"
+      patch "/update/:id", action: :update, as: "update_users"
+      get "/show/:id", action: :show, as: "show_users"
+      delete "/delete/:id", action: :destroy, as: "delete_users"
+    end
+  end
+end
